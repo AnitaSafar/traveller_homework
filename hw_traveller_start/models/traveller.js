5941a6dcf2ca5e6842fa710066d8fdfb0c2f7cd5
Anita Safar <ancsa0416@gmail.com>
2021-11-17 15:51:13 +0000

    first 4 code written, tests passing

---
+++ hw_traveller_start/models/traveller.js
@@ -0,0 +1,44 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  const start = [];
+  this.journeys.forEach((journey) => {
+    start.push(journey.startLocation)
+  });
+  return start;
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  const end = [];
+  this.journeys.forEach((journey) => {
+    end.push(journey.endLocation)
+  });
+  return end;
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+ const vehicle = this.journeys.filter((journey) => {
+  return journey.transport === transport;
+ });
+ return vehicle;
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  const distance = this.journeys.filter((journey) => {
+    return journey.distance > minDistance;
+  });
+  return distance;
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
